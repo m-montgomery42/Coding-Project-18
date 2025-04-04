808188d8576f7d47e5d30e9baa4c80bde075ab34
Markayla Montgomery <mm42@usf.edu>
2025-04-04 18:15:47 -0400

    Component Creation and Integration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Greeting from './components/Greeting';
+
+function App() {
+    return (
+    <>
+        <h1>Vite + React</h1>
+        <Greeting />
+    </>
+    );
+}
+
+export default App;
